fc4f8c5c46d3ce235ca881bae52a76491ac8d684
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2015-04-27 14:01:21 +1000

    Progress with the split of GustyLib into GustyKit and GustyAppKit.

--- GustyKit/GustyKit/CoreUI/classes/GustyKit.h
+++ GustyKit/GustyKit/CoreUI/classes/GustyKit.h
@@ -22,7 +22,6 @@
 //wip: document framework support and restrictions on GitHub
 //wip: document how to use it as framework in app extensions
 //wip: need to release the world when the framework work is finished (including testing the latest changes with the demo app)
-//wip: does the localisable stuff need to be split into kit and appKit too?
 //wip: for the classes that are paired in both frameworks, add a note about this fact in the class documentation
 
 // Foundation
