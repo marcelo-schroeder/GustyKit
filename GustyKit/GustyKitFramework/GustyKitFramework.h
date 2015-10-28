d9d32d9260d3263ed43cd7e5e8ec1e78d7174d5d
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2015-10-29 07:32:43 +1100

    Changes for Carthage compatibility

---
+++ GustyKit/GustyKitFramework/GustyKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  GustyKitFramework.h
+//  GustyKitFramework
+//
+//  Created by Marcelo Schroeder on 29/10/2015.
+//  Copyright © 2015 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GustyKitFramework.
+FOUNDATION_EXPORT double GustyKitFrameworkVersionNumber;
+
+//! Project version string for GustyKitFramework.
+FOUNDATION_EXPORT const unsigned char GustyKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GustyKitFramework/PublicHeader.h>
+
+
